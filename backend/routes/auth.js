1fa3d7a7c8476fde5c331be4b9022242daa680ff
niraj <nirajbhayera4@gmail.com>
2026-09-24 11:15:08 +0530

    updated auth.js
    Removed the bcryptjs with bcrypt and updated the server.js to use the auth route.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const router=require('express').Router()
-const bcrypt=require('bcryptjs')
+const bcrypt=require('bcrypt')
 const pool=require('../config/db')
 
 router.post('/signup', async(req,res)=>{
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,28 @@
-const express=require('express');
-const cors =require('cors');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const pool = require('./config/db');
 
-require('dotenv').config(); // loads the .env file in process.env by default 
-
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res) =>{
-    res.send('API is running');
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
-const PORT=process.env.PORT || 5000;
-app.listen(PORT, ()=>
-    console.log(`server is running on port ${PORT}`)
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ success: true, time: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`server is running on port ${PORT}`)
 );
-app.get('/test-db', async(req,res)=>{
-    try{
-        const result=await pool.query ('SELECT NOW()');
-        res.json({
-            success:true,
-            time:result.rows[0]
-        });
-    }
-    catch(err){
-        res.status(500).json({
-            success:false,
-            error:err.message
-        });
-    }
-});
