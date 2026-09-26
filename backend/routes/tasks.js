0667c3516390cd290860935d6c60fef941952829
niraj <nirajbhayera4@gmail.com>
2026-09-26 21:09:26 +0530

    updated the route of task.js
    Added the route with PUT HTTP method to update the task and update the DB.

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -41,3 +41,27 @@ router.post('/',authMiddleware, async(req,res)=>{
     }
 });
 
+// update the task 
+router.put('/', authMiddleware, async(req,res)=>{
+    try{
+        const {title,description,status}=req.body;
+        const result=await pool.query(
+            'UPDATE tasks SET title=$1, description=$2, status=$3 WHERE id=$4 AND user_id=$5 RETURNING*', 
+            [title, description, status , req.params.id, req.user.id]
+        );
+        if(result.rows.length===0){
+            return res.status(404).json({
+                error:'Task not found'
+            });
+        }
+        res.json(result.rows[0]);
+
+
+    }
+    catch(err){
+        res.status(500).json({
+            error:err.message
+        });
+
+    }
+})
